3d1816d4c00a91ec947de2f07f27be14cc852521
Muhammad Umair <umairmateen55@gmail.com>
2024-04-25 18:36:52 +0500

    dockerized

--- store/settings.py
+++ store/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&n1bwg$*@c%hs72uah+pvni2if5h(pw$tkny!gnfeevo5yw77p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
